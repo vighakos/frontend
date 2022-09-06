865aa662dd29b41f437b33b0d4ac76392640ae3a
vighakos <vighakos@turr.hu>
2022-09-06 13:59:13 +0200

    .

--- web/eletkor/js/app.js
+++ web/eletkor/js/app.js
@@ -1,11 +1,11 @@
 let form = document.querySelector('form'),
     button = document.querySelector('#button'),
     gombok = document.getElementsByTagName('input'),
-    arr = Array.prototype.slice.call( gombok ),
-    eletkor = 72
-
-
+    arr = Array.prototype.slice.call( gombok )
+    
+    
 button.addEventListener('click', () => {
+    eletkor = 72
     arr.forEach(element => {
         if (element.checked && element.value != "") {
             eletkor += parseInt(element.value)
