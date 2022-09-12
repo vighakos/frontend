44f146cbd2cafd862bed819b6b578bda995debc5
vighakos <vighakos@turr.hu>
2022-09-12 13:02:31 +0200

    panzio

--- panzio/js/app.js
+++ panzio/js/app.js
@@ -23,6 +23,10 @@ felveszBtn.addEventListener('click', () => {
     {
         alert('A mezők kitöltése kötelező!')
     } else {
+        let date1 = new Date(erkez.value)
+        let date2 = new Date(tavoz.value)
+        let diffTime = Math.abs(date2 - date1)
+        let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
         foglalas = {
             'id': foglalasok.length + 1,
             'erkezes': erkez.value,
@@ -35,7 +39,8 @@ felveszBtn.addEventListener('click', () => {
             'vendeg4_kor': parseInt(v4.value) != 0 && vendegszam.value > 3 ? v4.value : null,
             'ellatas': getValue(ellatas),
             'furdo': furdoHozzafer(),
-            'egyeb': egyeb.value
+            'egyeb': egyeb.value,
+            'ar': calcPrice(diffDays, getValue(szobak), vendegszam.value, getValue(ellatas), furdoHozzafer())
         }
         foglalasok.push(foglalas)
         localStorage.setItem('foglalasok', JSON.stringify(foglalasok))
@@ -57,4 +62,56 @@ function furdoHozzafer() {
         if (furdok[i].checked) valasz += furdok[i].value + ";"
     }
     return valasz
+}
+
+function calcPrice(diffDays, szoba, vendegek, ellatas, furdo) {
+    let szoba_ej = 0
+    switch (szoba) {
+        case 'egy_agy':
+            szoba_ej = 9000
+            break;
+    
+        case 'ket_agy':
+            szoba_ej = 15000
+            break;
+    
+        case 'ket_1pot':
+            szoba_ej = 18000
+            break;
+    
+        case 'ket_2pot':
+            szoba_ej = 21000
+            break;
+    }
+    console.log(szoba_ej);
+
+    let ellatas_ej = 0
+    switch (ellatas) {
+        case 'reggeli':
+            ellatas_ej = 900
+            break;
+    
+        case 'fel':
+            ellatas_ej = 2900
+            break;
+    
+        case 'teljes':
+            ellatas_ej = 4900
+            break;
+    }
+    console.log(ellatas_ej);
+
+    let furdo_ej = 0
+    if (furdo == "teljes") {
+        furdo_ej = 2000
+    } else {
+        let db = furdo.split(';').length
+        furdo_ej = (db-1)*800
+    }
+
+    console.log(furdo_ej);
+
+    console.log(szoba_ej + ellatas_ej + furdo_ej) * vendegek * diffDays
+
+    return (szoba_ej + ellatas_ej + furdo_ej) * vendegek * diffDays
 }
