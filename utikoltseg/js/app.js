534dd6509d77b2a4c7d1122ef92ab15dbefc84f2
vighakos <vighakos@turr.hu>
2022-09-13 14:04:12 +0200

    .

--- utikoltseg/js/app.js
+++ utikoltseg/js/app.js
@@ -13,19 +13,40 @@ let form = document.querySelector('form'),
     tavolsag = form.tavolsag,
     utikoltseg = document.querySelector('#utikoltseg'),
     amor = document.querySelector('#amor'),
-    osszeskoltseg = document.querySelector('#osszeskoltseg')
+    osszeskoltseg = document.querySelector('#osszeskoltseg'),
+    submitBtn = document.querySelector('#submit')
+
+submitBtn.addEventListener('click', () => {
+    if (nev.value == "" ||
+        lakcim.value == "" || 
+        szul.value == "" || 
+        tipus.value == "" || 
+        datum.value == "" || 
+        egysegar.value == "" || 
+        indul.value == "" || 
+        cel.value == "" || 
+        tovabbicel.value == "" || 
+        tavolsag.value == "") 
+    {
+        console.log('nem jó:DDD');   
+    } else {
+        let utikoltseg_szam = parseFloat(fogyasztas.value) * parseFloat(egysegar.value) * parseFloat(tavolsag.value) / 100
+        console.log(utikoltseg_szam);        
+        utikoltseg.innerText = utikoltseg_szam + 'Ft'
+    }
+})
 
 uzemanyag.addEventListener('change', () => {
     if (uzemanyag.value == 'benzin') {
-        fogyasztas.innerHTML = `<option value="1" selected>1000 cm3-ig 7,6 l/100km</option>
-            <option value="2">1001-1500 cm3 között 8,6 l/100km</option>
-            <option value="3">1501-2000 cm3 között 9,5 l/100km</option>
-            <option value="4">2001-3000 cm3 között 11,4 l/100km</option>
-            <option value="5">3001 cm3 fölött 13,3 l/100km </option>`
+        fogyasztas.innerHTML = `<option value="7.6" selected>1000 cm3-ig 7,6 l/100km</option>
+        <option value="8.6">1001-1500 cm3 között 8,6 l/100km</option>
+        <option value="9.5">1501-2000 cm3 között 9,5 l/100km</option>
+        <option value="11.4">2001-3000 cm3 között 11,4 l/100km</option>
+        <option value="13.3">3001 cm3 fölött 13,3 l/100km </option>`
     } else {
-        fogyasztas.innerHTML = `<option value="1" selected>1500 cm3-ig 5,7 l/100km</option>
-            <option value="2">1501-2000 cm3 között 6,7 l/100km</option>
-            <option value="3">2001-3000 cm3 között 7,6 l/100km</option>
-            <option value="4">3001 cm3 fölött 9,5 l/100km </option>`
+        fogyasztas.innerHTML = `<option value="5.7" selected>1500 cm3-ig 5,7 l/100km</option>
+            <option value="6.7">1501-2000 cm3 között 6,7 l/100km</option>
+            <option value="7.6">2001-3000 cm3 között 7,6 l/100km</option>
+            <option value="9.5">3001 cm3 fölött 9,5 l/100km </option>`
     }
 })
