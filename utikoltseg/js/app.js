0d543938f068fe04b46f2db554dff876da5001e9
vighakos <vighakos@turr.hu>
2022-09-13 17:37:59 +0200

    .

--- utikoltseg/js/app.js
+++ utikoltseg/js/app.js
@@ -6,7 +6,7 @@ let form = document.querySelector('form'),
     uzemanyag = form.uzemanyag,
     fogyasztas = form.fogyasztas,
     datum = form.datum,
-    egysegar = form.egysegar,
+    egysegar = form.ar,
     indul = form.indul,
     cel = form.cel,
     tovabbicel = form.tovabbicel,
@@ -14,7 +14,8 @@ let form = document.querySelector('form'),
     utikoltseg = document.querySelector('#utikoltseg'),
     amor = document.querySelector('#amor'),
     osszeskoltseg = document.querySelector('#osszeskoltseg'),
-    submitBtn = document.querySelector('#submit')
+    submitBtn = document.querySelector('#submit'),
+    nyomiBtn = document.querySelector('#nyomi')
 
 submitBtn.addEventListener('click', () => {
     if (nev.value == "" ||
@@ -28,21 +29,25 @@ submitBtn.addEventListener('click', () => {
         tovabbicel.value == "" || 
         tavolsag.value == "") 
     {
-        console.log('nem jó:DDD');   
+        alert('Tölts ki minden adatot!')  
     } else {
-        let utikoltseg_szam = parseFloat(fogyasztas.value) * parseFloat(egysegar.value) * parseFloat(tavolsag.value) / 100
-        console.log(utikoltseg_szam);        
-        utikoltseg.innerText = utikoltseg_szam + 'Ft'
+        let utikoltseg_szam = Math.round(parseFloat(fogyasztas.value) * parseFloat(egysegar.value) * parseFloat(tavolsag.value) / 100)
+        utikoltseg.innerText = utikoltseg_szam + ' Ft'
+        amor.innerText = tavolsag.value * 15 + ' Ft'
+        osszeskoltseg.innerText = utikoltseg_szam + tavolsag.value * 15 + ' Ft'
+        nyomiBtn.disabled = false;
     }
 })
+
+nyomiBtn.addEventListener('click', () => { window.print() })
 
 uzemanyag.addEventListener('change', () => {
     if (uzemanyag.value == 'benzin') {
         fogyasztas.innerHTML = `<option value="7.6" selected>1000 cm3-ig 7,6 l/100km</option>
-        <option value="8.6">1001-1500 cm3 között 8,6 l/100km</option>
-        <option value="9.5">1501-2000 cm3 között 9,5 l/100km</option>
-        <option value="11.4">2001-3000 cm3 között 11,4 l/100km</option>
-        <option value="13.3">3001 cm3 fölött 13,3 l/100km </option>`
+            <option value="8.6">1001-1500 cm3 között 8,6 l/100km</option>
+            <option value="9.5">1501-2000 cm3 között 9,5 l/100km</option>
+            <option value="11.4">2001-3000 cm3 között 11,4 l/100km</option>
+            <option value="13.3">3001 cm3 fölött 13,3 l/100km </option>`
     } else {
         fogyasztas.innerHTML = `<option value="5.7" selected>1500 cm3-ig 5,7 l/100km</option>
             <option value="6.7">1501-2000 cm3 között 6,7 l/100km</option>
