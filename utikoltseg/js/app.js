029544737c1c3e165ea711190b7f49fd366c0c03
vighakos <vighakos@turr.hu>
2022-09-13 10:53:55 +0200

    .

--- utikoltseg/js/app.js
+++ utikoltseg/js/app.js
@@ -24,8 +24,8 @@ uzemanyag.addEventListener('change', () => {
             <option value="5">3001 cm3 fölött 13,3 l/100km </option>`
     } else {
         fogyasztas.innerHTML = `<option value="1" selected>1500 cm3-ig 5,7 l/100km</option>
-        <option value="2">1501-2000 cm3 között 6,7 l/100km</option>
-        <option value="3">2001-3000 cm3 között 7,6 l/100km</option>
-        <option value="4">3001 cm3 fölött 9,5 l/100km </option>`
+            <option value="2">1501-2000 cm3 között 6,7 l/100km</option>
+            <option value="3">2001-3000 cm3 között 7,6 l/100km</option>
+            <option value="4">3001 cm3 fölött 9,5 l/100km </option>`
     }
 })
