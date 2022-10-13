9e4178b179c89b022b9182cce528a5dab15b1ae9
vighakos <vighakos@turr.hu>
2022-10-13 12:29:01 +0200

    sziasztok

--- angular/weather/js/weather.js
+++ angular/weather/js/weather.js
@@ -5,33 +5,38 @@ app.controller('mainCtrl',function($scope){
     $scope.company='Bajai Szc Türr István Technikum'
     $scope.author='2/14 SZFT'
     $scope.idojarasok=['napsütés','borult','eső','jégeső','havazás']
-    
-    $scope.today = moment(new Date()).format('YYYY-MM-DD')
     $scope.elorejelzes = {}
 
-    $scope.adatok=angular.fromJson( window.localStorage.getItem('weatherforecast'))
+    $scope.today = moment(new Date()).format('YYYY-MM-DD')
+    $scope.adatok = angular.fromJson( window.localStorage.getItem('weatherforecast'))
 
     if($scope.adatok==null) $scope.adatok=[]
 
     $scope.hozzaadas=function(){
-        if(elorejelzes.min==null || 
-            elorejelzes.max==null ||
-            elorejelzes.datum==null ||
-            elorejelzes.idojaras==null)
+        if($scope.elorejelzes.datum==null ||
+            $scope.elorejelzes.min==null || 
+            $scope.elorejelzes.max==null ||
+            $scope.elorejelzes.idojaras==null)
         {
             $scope.message='Nem adott meg minden szükséges adatot'
         } else {
             $scope.adatok.push(
                 {
-                    datum:forecast.datum,
-                    min:forecast.min,
-                    max:forecast.max,
-                    idojaras:forecast.idojaras
+                    datum:moment($scope.elorejelzes.datum).format('LL'),
+                    min:$scope.elorejelzes.min,
+                    max:$scope.elorejelzes.max,
+                    idojaras:$scope.elorejelzes.idojaras
                 }
             )
 
-            window.localStorage.setItem('weatherforecast', adatok.toJson($scope.adatok))
-            $scope.elorejelzes={}
+            window.localStorage.setItem('weatherforecast', angular.toJson($scope.adatok))
+            $scope.elorejelzes = {}
         }
+    }
+
+    $scope.torles = function(ID) {
+        let idx = $scope.adatok.findIndex(item => item.ID == ID);
+        $scope.adatok.splice(idx, 1);
+        window.localStorage.setItem('weatherforecast', angular.toJson($scope.adatok))
     }
 })
